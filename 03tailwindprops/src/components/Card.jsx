5926d18ca2d2aa7fdbed5beb75024b9c80fd38a1
rohit <royyo8226@gmail.com>
2026-08-12 23:09:35 +0530

    Add Tailwind CSS setup

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import'./Card.css'
+
+function Card({userName="ansh",ok='knowMore'}) {
+
+  return (
+    <div>
+            <div class="card">
+            <div class="header">
+						<span class="title">Beginner</span>
+						<span class="price">Free</span>
+					</div>
+					<p class="desc">{userName}</p>
+					<ul class="lists">
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Aenean quis</span>
+						</li>
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Morbi semper</span>
+						</li>
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Tristique enim nec</span>
+						</li>
+					</ul>
+				<button type="button" class="action">{ok}</button>
+	        </div>
+    </div>
+  )
+}
+
+export default Card
