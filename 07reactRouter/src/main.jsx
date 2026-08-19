d86a60aa639ac482fc679cb9db32471aa5c8e4b5
rohit <royyo8226@gmail.com>
2026-08-20 00:20:12 +0530

    add miniContext

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -13,7 +13,7 @@ const router=createBrowserRouter(
     <Route path='/'element ={<Layout/>}>
       <Route path=''element ={<Home/>}></Route>
       <Route path='about'element={<About/>}></Route>
-      <Route path='contact'element={<Element/>}></Route>
+      <Route path='contact'element={<Contact/>}></Route>
 
       
 
